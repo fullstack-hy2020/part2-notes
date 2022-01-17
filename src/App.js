03ee8c7d3fbd7ea66ebcb8cccd0343583637530a
Matti Luukkainen <mluukkai@iki.fi>
2022-01-17 16:01:47 +0200

    part2-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes } ) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('') 
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +31,13 @@ const App = ({ notes } ) => {
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
