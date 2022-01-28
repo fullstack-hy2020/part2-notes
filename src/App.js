c51d7ba0ac4127fa80026d4d87b1d38fe2592802
Matti Luukkainen <mluukkai@iki.fi>
2022-01-28 10:53:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -98,39 +98,39 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-    const loginForm = () => (
-      <form onSubmit={handleLogin}>
-        <div>
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
           username
-            <input
+          <input
             type="text"
             value={username}
             name="Username"
             onChange={({ target }) => setUsername(target.value)}
           />
-        </div>
-        <div>
+      </div>
+      <div>
           password
-            <input
+          <input
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
-        </div>
-        <button type="submit">login</button>
-      </form>      
-    )
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
   
-    const noteForm = () => (
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>  
-    )
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
 
   return (
     <div>
