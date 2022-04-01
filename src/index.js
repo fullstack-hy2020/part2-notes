e83b6bf42977904f9109335ab9c5044aad33b6fe
Sebastian Vera <100543895+verajs@users.noreply.github.com>
2022-04-01 00:48:15 -0400

    Unnecessary get request in index.js removed, no props in App being used.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.render(
-    <App notes={notes} />,
-    document.getElementById('root')
-  )
-})
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
