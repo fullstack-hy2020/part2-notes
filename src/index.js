b8ec2f14502ebd8414124421fed0619a1bed75b0
Matti Luukkainen <mluukkai@iki.fi>
2023-01-16 20:23:03 +0200

    p2-7

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
-
 import App from './App'
+import './index.css'
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-})
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
