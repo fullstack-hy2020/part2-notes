b9ff8bb8af29bf2ada7a89afbc5661d141e38af4
Matti Luukkainen <mluukkai@iki.fi>
2023-01-16 19:10:56 +0200

    fix

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,6 +1,12 @@
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important
+    ? 'make not important' : 'make important'
+
   return (
-    <li>{note.content}</li>
+    <li>
+      {note.content} 
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
   )
 }
 
