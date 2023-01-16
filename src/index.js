06604a24456493ff7e5d326e69081fc8bc8b20f3
Matti Luukkainen <mluukkai@iki.fi>
2023-01-16 11:32:33 +0200

    part2-4

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
-import ReactDOM from "react-dom/client"
+import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
-import App from "./App"
+import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
