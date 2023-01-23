0e007f25e66bed3c9e3343ccc943b71dc5bdb820
Matti Luukkainen <mluukkai@iki.fi>
2023-01-23 22:41:53 +0200

    part5-10

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000'
   },
-});
+  env: {
+    BACKEND: 'http://localhost:3001/api'
+  }
+})
