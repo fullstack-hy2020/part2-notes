1ae011e092ca44b4b5d71a213cfdead1b47998e3
Matti Luukkainen <mluukkai@iki.fi>
2023-01-23 17:55:54 +0200

    tweak

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -7,7 +7,7 @@ const NoteForm = ({ createNote }) => {
     event.preventDefault()
     createNote({
       content: newNote,
-      important: Math.random() > 0.5,
+      important: true,
     })
 
     setNewNote('')
