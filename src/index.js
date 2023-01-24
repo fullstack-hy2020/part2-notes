7e6cbd1c1d1a53fb419fdb997a7a3d204740ed55
pablo-maff <pablo.maff.076@gmail.com>
2023-01-24 23:04:31 +0100

    Removed old axios call in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import axios from 'axios'
-
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes} />
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
